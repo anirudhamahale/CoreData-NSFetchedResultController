a67fcb9ed7777dcf5f364c66c7ab7a1a08ea6378
Anirudha <anirudhamahale@gmail.com>
2016-12-21 15:34:16 +0530

    first commit

---
+++ Done/TodoCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoCell.swift
+//  Done
+//
+//  Created by LA Argon on 12/21/16.
+//  Copyright © 2016 com.letsappit. All rights reserved.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
